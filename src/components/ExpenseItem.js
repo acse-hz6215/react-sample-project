1b78868c64d839d88d00037f3e2b6923ed4123b7
hzmacso <hanxiao.zhang@macso.ai>
2023-07-05 11:27:21 +1200

    react sample project

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,14 @@
+function ExpenseItem()
+{
+    return (
+        <div>
+            <div>march 28</div>
+            <div>
+                <h2>Car insurance </h2>
+                <div>20.0</div>
+            </div>
+        </div>
+    );
+}
+
+export default ExpenseItem;
