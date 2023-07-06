fb3ecb00d057afe9b2a50d8fe1aef797d95a60f6
hzmacso <hanxiao.zhang@macso.ai>
2023-07-06 13:58:54 +1200

    sec48:passing data via props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,21 +2,18 @@
    For commenting:
    - Use "//" for regular JavaScript single-line comments.
    - Use "{/* *'/}" within JSX structure for comments.
-   */
-
+*/
 
 import "./ExpenseItem.css";
 
-function ExpenseItem()
+function ExpenseItem(props)
 {
-    const expenseTitle = 'Car Insurance';
-
     return (
         <div className="expense-item">
-            <div>{expenseDate.toISOString()} </div>  {/* toISOString() method is a built-in method of the Date object*/}
+            <div>{props.date.toISOString()} </div>
             <div className="expense-item__description">
-                <h2>{expenseTitle}</h2>
-                <div className="expense-item__price">20.0</div>
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">{props.amount}</div>
             </div>
         </div>
     );
