ad2d9eef6d0253da5c52ca801433a38fd2c391ba
hzmacso <hanxiao.zhang@macso.ai>
2023-07-06 12:53:57 +1200

    update code

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,22 @@
+/*
+   For commenting:
+   - Use "//" for regular JavaScript single-line comments.
+   - Use "{/* *'/}" within JSX structure for comments.
+   */
+
+
+import "./ExpenseItem.css";
+
 function ExpenseItem()
 {
+    const expenseTitle = 'Car Insurance';
+
     return (
-        <div>
-            <div>march 28</div>
-            <div>
-                <h2>Car insurance </h2>
-                <div>20.0</div>
+        <div className="expense-item">
+            <div>{expenseDate.toISOString()} </div>  {/* toISOString() method is a built-in method of the Date object*/}
+            <div className="expense-item__description">
+                <h2>{expenseTitle}</h2>
+                <div className="expense-item__price">20.0</div>
             </div>
         </div>
     );
